9d612c0bc6ff9fe3552b0c9ddf17fd4c92d64467
dannygallows <43937866+dannygallows@users.noreply.github.com>
2024-11-01 22:28:25 +0100

    Complete 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,22 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...itemsToRemove) {
+    
+    let resultArray = [];
+    let lengthOfRemoveArray = itemsToRemove.length;
+    
+    for (item of array) {
+        
+        for (let i = 0; i < lengthOfRemoveArray; i++) {
+ 
+            if (item == itemsToRemove[i] && (typeof(item) == typeof(itemsToRemove[i]))) {
+                break;
+            }
+                
+            if (i == lengthOfRemoveArray - 1) {
+                resultArray.push(item);
+            }
+        }
+    }
+    return resultArray;
 };
 
 // Do not edit below this line
