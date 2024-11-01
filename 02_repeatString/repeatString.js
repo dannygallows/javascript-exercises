1e57a53d6cff4cb4490434eb9f1547f206cc0684
dannygallows <43937866+dannygallows@users.noreply.github.com>
2024-11-01 20:52:53 +0100

    Complete 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    
+    if (num < 0) 
+    {
+        return 'ERROR';
+    }
 
+    let helperString = "";
+    for (let i = 0; i < num; i++) 
+    {
+        helperString += string;
+    }
+    return helperString;
 };
 
 // Do not edit below this line
