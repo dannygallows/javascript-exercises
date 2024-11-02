e9732c9e572a0f34215f3c9268f44d94de00bb52
dannygallows <43937866+dannygallows@users.noreply.github.com>
2024-11-02 11:17:01 +0100

    Complete 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(from, to) {
+    
+    let finalSum = 0;  
+    let count = 0;
+    
+    if (from > to) {
+        count = from;
+    }
+    else count = to;
 
+    if (!Number.isInteger(from) || !Number.isInteger(to) || from < 0 || to < 0) {
+        return "ERROR";
+    }
+
+    
+    for (let i = 1; i <= count; i++) {
+        finalSum += i;
+    }
+
+    return finalSum;
 };
 
 // Do not edit below this line
