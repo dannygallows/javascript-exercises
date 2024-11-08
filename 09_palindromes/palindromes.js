5e9f101eb725dd71628bb8069a3d71b56b75bb58
dannygallows <43937866+dannygallows@users.noreply.github.com>
2024-11-08 20:33:46 +0100

    Complete 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,21 @@
-const palindromes = function () {
+const palindromes = function (inputString) {
+    
+    let string = inputString.toLowerCase();
+    string = string.replace(/[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~ ]/g, '');
+    
+    let strlen = string.length;
+    
+    for (let i = 0; i < strlen; i++) {
+        strlen--;
 
+        if (string.charAt(i) != string.charAt(strlen)) {
+            return false;
+        }
+    }
+    return true;
 };
+
+
 
 // Do not edit below this line
 module.exports = palindromes;
