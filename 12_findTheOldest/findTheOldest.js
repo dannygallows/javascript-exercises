80945f291ebc1b66849aca21d62cb8453c8d08f9
dannygallows <43937866+dannygallows@users.noreply.github.com>
2024-11-08 21:48:04 +0100

    Complete 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
 
+    array.forEach(element => {
+        if (!element.yearOfDeath) {
+            element.yearOfDeath = 2024;
+        }
+    });
+
+    let userAges = array.map(item => (
+    {
+        name: item.name,
+        age : item.yearOfDeath - item.yearOfBirth
+    }));
+
+    let sorted = userAges.sort( (a,b) => b.age - a.age);
+
+    return sorted[0];
 };
 
 // Do not edit below this line
