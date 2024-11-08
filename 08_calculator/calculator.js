80ee257a3774c0870be02767c217334014ffa195
dannygallows <43937866+dannygallows@users.noreply.github.com>
2024-11-08 19:32:36 +0100

    Complete 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(a, b) {
+return a + b	
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+return a - b	
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+return array.reduce((sum, currentItem) => {
+return sum + currentItem;
+}, 0	
+)};
+
+const multiply = function(array) {
+  return array.reduce((total, currentItem) => {
+  return total * currentItem;
+  }, 1	
+  )};
+
+
+const power = function(x, n) {
+  let total = 1;
+  for (let i = 0; i < n ; i++) {
+    total *= x;
+  }
+  return total;
 };
 
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(x) {
+  let total = 1;
+  for (let i = 0; i < x; i++)
+  {
+    total *= x - i; 
+  }
+  return total;
 };
 
 // Do not edit below this line
